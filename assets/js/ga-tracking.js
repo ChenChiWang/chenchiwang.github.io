e149c0066ce7fc5f7f5e7463cc7bb913c134180d
chenchiwang <fantastic.core@gmail.com>
2024-11-24 20:06:56 +0800

    Add a nuacne on heads

---
+++ assets/js/ga-tracking.js
@@ -0,0 +1,14 @@
+// assets/js/ga-tracking.js
+
+(function() {
+    var gtagScript = document.createElement('script');
+    gtagScript.async = true;
+    gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-SF5KP87108";
+    document.head.appendChild(gtagScript);
+  
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-SF5KP87108');
+  })();
+  
